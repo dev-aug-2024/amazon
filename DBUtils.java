9ea1f0af59ea937b9f24f46278cad1e11bd03245
Niharika-palle <ec2-user@ip-172-31-45-45.ap-south-1.compute.internal>
2024-09-05 13:16:02 +0000

    updated new one

--- DBUtils.java
+++ DBUtils.java
@@ -2,3 +2,4 @@ This file contains the DB connection logic
 hello updating the line bye
 HELLO UPDATEING THIS LINE
 hi there updating again
+SEE YOU
--- Deploy.java
+++ Deploy.java
@@ -1,4 +1,4 @@
 This file contains the deployment logic
 hi there
 hello updated
-hi there i am updating the data
+hi there i am updating the data again.
