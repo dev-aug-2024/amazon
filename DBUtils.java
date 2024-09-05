2b58cd1158db034679285c46b8f4a766b329a09d
Niharika-palle <ec2-user@ip-172-31-45-45.ap-south-1.compute.internal>
2024-09-05 13:16:02 +0000

    abc

--- DBUtils.java
+++ DBUtils.java
@@ -1,3 +1,8 @@
 This file contains the DB connection logic
 hello updating the line bye
 HELLO UPDATEING THIS LINE
+<<<<<<< HEAD
+=======
+hi there updating again
+SEE YOU
+>>>>>>> 9ea1f0a (updated new one)
--- Deploy.java
+++ Deploy.java
@@ -1,3 +1,7 @@
 This file contains the deployment logic
 hi there
 hello updated
+<<<<<<< HEAD
+=======
+hi there i am updating the data again.
+>>>>>>> 9ea1f0a (updated new one)
